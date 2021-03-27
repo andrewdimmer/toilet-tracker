695e7ee45bd2e7750a0b1d28faa0dfa6639f0862
Nathan Dimmer <nathandimmer@gmail.com>
2021-03-27 15:04:39 -0400

    Changed Information Pane UI

--- flutter/lib/widgets/poiCapacityBar.dart
+++ flutter/lib/widgets/poiCapacityBar.dart
@@ -28,6 +28,11 @@ class _PoiCapacityBar extends State<PoiCapacityBar> {
   int _crowd = 0;
   String _capacity = "?";
 
+  String _newEditConfirm = "new";
+  int _stars = 0;
+  bool _open = false;
+  bool _fee = false;
+
   void _processNewCrowdAndCapacityData(DocumentSnapshot snapshot) {
     setState(() {
       _crowd = snapshot.exists ? snapshot.data()["crowd"] : 0;
@@ -44,25 +49,187 @@ class _PoiCapacityBar extends State<PoiCapacityBar> {
         ),
       );
 
+  void _setNewEdit() {
+    setState(() {
+      if (_newEditConfirm == "new") {
+        _newEditConfirm = "confirm";
+      } else if (_newEditConfirm == "edit") {
+        _newEditConfirm = "confirm";
+      } else if (_newEditConfirm == "confirm") {
+        _newEditConfirm = "edit";
+      }
+    });
+  }
+
+  void _setStars(int numberOfStars) {
+    if (_newEditConfirm == "confirm") {
+      setState(() {
+        _stars = numberOfStars;
+      });
+    }
+  }
+
+  void _setOpen() {
+    if (_newEditConfirm == "confirm") {
+      setState(() {
+        _open = !_open;
+      });
+    }
+  }
+
+  void _setFee() {
+    if (_newEditConfirm == "confirm") {
+      setState(() {
+        _fee = !_fee;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) => Container(
         child: _busyLoading
             ? loadingWheelAndMessage("Loading Crowd and Capacity Data...")
             : Column(
                 children: <Widget>[
-                  Center(
-                    child: Text(_crowd.toString() + " / " + _capacity,
-                        style: TextStyle(fontSize: 24)),
-                  ),
                   ButtonBar(
                     children: <Widget>[
-                      ElevatedButton(
-                        child:
-                            Text("Know the capacity? Update the max capacity!"),
-                        onPressed: _openUpdateCapacity,
+                      ClipOval(
+                        child: Material(
+                          color: Colors.brown, // button color
+                          child: InkWell(
+                            splashColor: Colors.green, // inkwell color
+                            child: SizedBox(
+                                width: 56,
+                                height: 56,
+                                child: Icon(
+                                  (_newEditConfirm == "new")
+                                      ? Icons.add
+                                      : (_newEditConfirm == "edit")
+                                          ? Icons.edit
+                                          : (_newEditConfirm == "confirm")
+                                              ? Icons.check
+                                              : Icons.block,
+                                  color: Colors.white,
+                                )),
+                            onTap: () {
+                              _setNewEdit();
+                            },
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 290.0,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            ButtonBar(
+                              alignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                IconButton(
+                                  icon: Icon((_stars >= 1)
+                                      ? Icons.star
+                                      : Icons.star_border),
+                                  onPressed: () {
+                                    _setStars(1);
+                                  },
+                                  color: (_stars >= 1)
+                                      ? Colors.yellow
+                                      : Colors.black,
+                                ),
+                                IconButton(
+                                  icon: Icon((_stars >= 2)
+                                      ? Icons.star
+                                      : Icons.star_border),
+                                  onPressed: () {
+                                    _setStars(2);
+                                  },
+                                  color: (_stars >= 2)
+                                      ? Colors.yellow
+                                      : Colors.black,
+                                ),
+                                IconButton(
+                                  icon: Icon((_stars >= 3)
+                                      ? Icons.star
+                                      : Icons.star_border),
+                                  onPressed: () {
+                                    _setStars(3);
+                                  },
+                                  color: (_stars >= 3)
+                                      ? Colors.yellow
+                                      : Colors.black,
+                                ),
+                                IconButton(
+                                  icon: Icon((_stars >= 4)
+                                      ? Icons.star
+                                      : Icons.star_border),
+                                  onPressed: () {
+                                    _setStars(4);
+                                  },
+                                  color: (_stars >= 4)
+                                      ? Colors.yellow
+                                      : Colors.black,
+                                ),
+                                IconButton(
+                                  icon: Icon((_stars >= 5)
+                                      ? Icons.star
+                                      : Icons.star_border),
+                                  onPressed: () {
+                                    _setStars(5);
+                                  },
+                                  color: (_stars >= 5)
+                                      ? Colors.yellow
+                                      : Colors.black,
+                                ),
+                              ],
+                            ),
+                            ButtonBar(
+                              alignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                ElevatedButton(
+                                  child: Text(_open ? "Open" : "Closed"),
+                                  onPressed: () {
+                                    _setOpen();
+                                  },
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty
+                                        .resolveWith<Color>(
+                                      (Set<MaterialState> states) {
+                                        if (_open) {
+                                          return Colors.green;
+                                        } else if (!_open) {
+                                          return Colors.red;
+                                        }
+                                        return null; // Use the component's default.
+                                      },
+                                    ),
+                                  ),
+                                ),
+                                ElevatedButton(
+                                  child: Text(_fee ? "Free" : "Paid"),
+                                  onPressed: () {
+                                    _setFee();
+                                  },
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty
+                                        .resolveWith<Color>(
+                                      (Set<MaterialState> states) {
+                                        if (_fee) {
+                                          return Colors.green;
+                                        } else if (!_fee) {
+                                          return Colors.red;
+                                        }
+                                        return null; // Use the component's default.
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
                       )
                     ],
-                    alignment: MainAxisAlignment.center,
+                    alignment: MainAxisAlignment.spaceEvenly,
                   )
                 ],
               ),
